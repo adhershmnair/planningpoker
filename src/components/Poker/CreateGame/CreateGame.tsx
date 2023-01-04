e02e121c6508798633e1023661d44840ff3b0162
Adharsh M <adhershmnair@gmail.com>
2023-01-05 02:33:55 +0530

    Updated game config.

--- src/components/Poker/CreateGame/CreateGame.tsx
+++ src/components/Poker/CreateGame/CreateGame.tsx
@@ -106,7 +106,7 @@ export const CreateGame = () => {
               <FormControlLabel
                 value={GameType.Normal}
                 control={<Radio color='primary' size='small' />}
-                label='Normal (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 16, 18, 20)'
+                label='Normal (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15)'
               />
               <FormControlLabel
                 value={GameType.Fibonacci}
