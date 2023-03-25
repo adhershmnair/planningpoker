deae5ae17578ad5697b170be2caf19fbf6a76f23
Adharsh M <adhershmnair@gmail.com>
2023-03-26 02:03:50 +0530

    Updated logo and title.

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,14 +1,12 @@
 import { Button, Slide, useMediaQuery } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import AppToolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import GamesIcon from '@material-ui/icons/Games';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import MergeTypeOutlinedIcon from '@material-ui/icons/MergeTypeOutlined';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './Toolbar.css';
-export const title = 'Victoria Planning Poker';
+import LogoIconSvg from './../../images/vu-logo.svg';
 
 export const Toolbar = () => {
   const history = useHistory();
@@ -23,10 +21,11 @@ export const Toolbar = () => {
               className='HeaderLeftContainer'
               onClick={() => history.push('/')}
             >
-              <GamesIcon className='HeaderIcon' />
-              <Typography variant={isSmallScreen? 'subtitle1':'h5'} color='inherit' noWrap>
-                {title}
-              </Typography>
+              <img
+                    alt='Victoria University'
+                    className='LogoIcon'
+                    src={LogoIconSvg}
+                  ></img>
             </div>
             <div>
               <Button title="New Session" startIcon={<AddCircleOutlineIcon/>} color='inherit' onClick={() => history.push('/')}>
